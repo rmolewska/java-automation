7a82dd9c57af163b53e323a89efada6baafad098
Renata <keirka@gmail.com>
2019-12-15 22:28:05 +0100

    adding fucnctions

--- sandbox/src/main/java/qa/sandbox/FirstProgram.java
+++ sandbox/src/main/java/qa/sandbox/FirstProgram.java
@@ -3,8 +3,27 @@ package qa.sandbox;
 public class FirstProgram {
 	
 	public static void main(String[] args) {
+		hello("world");
+		hello("Keira");
 
-		String somebody = "world";
+		double len = 5;
+		System.out.println("Square area is: " + area(len));
+
+		double a = 6;
+		double b = 4;
+		System.out.println("Rectangle area is: " + area(a, b));
+
+	}
+	public static void hello(String somebody) {
 		System.out.println("Hello," + " " + somebody +  "!" );
 	}
+
+	public static double area(double l) {
+		return l * l;
+	};
+
+	public static double area(double a, double b) {
+		return a * b;
+	};
+
 }
